4f8115e8565216e3cf875102ae87933330ab9b2e
onisim_albarov <onisim93@gmail.com>
2022-12-06 12:15:06 +0400

    complete stage1-module5-task1

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,32 +1,40 @@
 package com.epam.mjc;
 
-import java.util.List;
-import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collectors;
 
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return strings -> {
+            for (String s : strings) {
+                char firstChar = s.charAt(0);
+                if (Character.isDigit(firstChar) || Character.isLowerCase(firstChar)) {
+                    return false;
+                }
+            }
+            return true;
+        };
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return numbers -> numbers.addAll(numbers.stream().filter(i-> i % 2 == 0).collect(Collectors.toList()));
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return  () -> values.stream().filter(s-> Character.isUpperCase(s.charAt(0))
+                && s.endsWith(".")
+                && s.split(" ").length > 3).collect(Collectors.toList());
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list) -> list.stream().collect(Collectors.toMap(s->s, String::length));
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (first, second) -> new ArrayList<>(first){{
+            addAll(second);
+        }};
     }
 }
